d3c92d3a7f827d3fc59ff1848ed6c71121c1d760
Tiago Rodrigues <isatiago098@gmail.com>
2021-09-03 09:44:24 -0300

    resolucao exercicio cebolinha

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Frasedigitada, Frasecomdislalia;
+            Console.WriteLine("digite uma flase");
+            Frasedigitada = Console.ReadLine();
+
+            Frasecomdislalia = Frasedigitada.Replace("r", "l").Replace("R","L");
+
+            Console.WriteLine(Frasecomdislalia);
+
+        }
+    }
+}
